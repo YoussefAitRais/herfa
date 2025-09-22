4f13ec0eb2cd56138cb7e6bb8b7cfd9b863fb103
Youssef Ait Rais <copybuisness007@gmail.com>
2025-09-22 13:03:35 +0200

    first commit

---
+++ backend/herfa/src/main/java/org/event/herfa/HerfaApplication.java
@@ -0,0 +1,13 @@
+package org.event.herfa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerfaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HerfaApplication.class, args);
+    }
+
+}
---
+++ backend/herfa/src/test/java/org/event/herfa/HerfaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.event.herfa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerfaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
