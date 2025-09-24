10495b5435faa12a17ec00e2c3b87b1675187d16
Youssef Ait Rais <copybuisness007@gmail.com>
2025-09-24 16:10:27 +0200

    Artisan Repository

--- backend/herfa/src/main/java/Repository/ArtisanRepository.java
+++ backend/herfa/src/main/java/Repository/ArtisanRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ArtisanRepository extends JpaRepository<Artisan, Long> {
 
+    Artisan findByArtisanId(Long artisanId);
 }
