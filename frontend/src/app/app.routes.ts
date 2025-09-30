6568caceebf51251ac49ff00b578e19afa3f236a
Youssef Ait Rais <copybuisness007@gmail.com>
2025-09-30 12:55:53 +0200

    creating components login and register

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,12 +1,24 @@
 import { Routes } from '@angular/router';
 import path from "node:path";
 import {HomeComponent} from "./component/home/home.component";
+import {LoginComponent} from "./component/login/login.component";
+import {RegisterComponent} from "./component/register/register.component";
 
 export const routes: Routes = [
   {
     path: '',
     component: HomeComponent
 
+  },
+
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+
+  {
+    path: 'register',
+    component: RegisterComponent
   }
 
 ];
---
+++ frontend/src/app/dashboard/artisan-dashboard/artisan-dashboard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-artisan-dashboard',
+  standalone: true,
+  imports: [],
+  templateUrl: './artisan-dashboard.component.html',
+  styleUrl: './artisan-dashboard.component.css'
+})
+export class ArtisanDashboardComponent {
+
+}
